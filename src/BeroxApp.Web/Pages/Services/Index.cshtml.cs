c1bbcfeb41437d66a810d1dc9a30069b74555fb8
Batuhan Karadere <bbkaradere@gmail.com>
2025-07-03 17:57:23 +0300

    Services index eklendi, localitazatonları eklendi.

--- src/BeroxApp.Web/Pages/Services/Index.cshtml.cs
+++ src/BeroxApp.Web/Pages/Services/Index.cshtml.cs
@@ -1,18 +1,18 @@
+// BeroxApp.Web/Pages/Services/Index.cshtml.cs
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
 using BeroxApp.Services;
 using Volo.Abp.Application.Dtos;
-using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
 
 namespace BeroxApp.Web.Pages.Services
 {
     public class IndexModel : AbpPageModel
     {
-        public string NameFilter { get; set; }
-        public bool? IsActiveFilter { get; set; }
-
         private readonly IServiceAppService _serviceAppService;
+
+        public PagedResultDto<ServiceDto> Services { get; set; }
 
         public IndexModel(IServiceAppService serviceAppService)
         {
@@ -21,7 +21,16 @@ namespace BeroxApp.Web.Pages.Services
 
         public async Task OnGetAsync()
         {
+            Services = await _serviceAppService.GetListAsync(new PagedAndSortedResultRequestDto
+            {
+                MaxResultCount = 20
+            });
+        }
 
+        public async Task<IActionResult> OnPostDeleteAsync(Guid id)
+        {
+            await _serviceAppService.DeleteAsync(id);
+            return NoContent();
         }
     }
 }
